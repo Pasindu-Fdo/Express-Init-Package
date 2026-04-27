5dd0efcfc16b2d0b6e1a796db44edbeaf08aff54
Pasindu Deshan <deshanfdo27@gmail.com>
2026-04-27 22:52:13 +0530

    fix: updated the prisma to version 7

--- addons/db-mysql-js/src/config/db.js
+++ addons/db-mysql-js/src/config/db.js
@@ -1,6 +1,24 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const prisma = new PrismaClient();
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const parsedUrl = new URL(databaseUrl);
+const adapter = new PrismaMariaDb({
+  host: parsedUrl.hostname,
+  port: parsedUrl.port ? Number(parsedUrl.port) : 3306,
+  user: decodeURIComponent(parsedUrl.username),
+  password: decodeURIComponent(parsedUrl.password),
+  database: decodeURIComponent(parsedUrl.pathname.replace(/^\//, "")),
+  connectionLimit: 5,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 export async function connectDB() {
   try {
--- addons/db-postgres-ts/src/config/db.ts
+++ addons/db-postgres-ts/src/config/db.ts
@@ -1,6 +1,16 @@
-import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient();
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const adapter = new PrismaPg({ connectionString: databaseUrl });
+
+const prisma = new PrismaClient({ adapter });
 
 export async function connectDB(): Promise<void> {
   try {
